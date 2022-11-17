e258e5f95286b67e0d72f77ad1e60ed6775304ce
dastanchick <dosyafree32@gmail.com>
2022-11-17 19:57:12 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner w = new Scanner(System.in);
+        int a = w.nextInt();
+        if (a == 1) {
+            System.out.println("Литература");
+
+        } else if (a == 2) {
+            System.out.println("Русский-язык");
+        } else {
+            System.out.println("Сабак болбойт");
+        }
+    }
+}
+
+    
